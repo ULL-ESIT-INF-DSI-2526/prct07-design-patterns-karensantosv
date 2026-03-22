e5297d85717e159d3922cc6aca5812ad5133b170
Karen Santos <alu0101677354@ull.edu.es>
2026-03-22 14:02:43 +0000

    template

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Función para sumar
- * @param a - primer número
- * @param b - segundo número
- * @returns - la suma de a y b
- */
-export declare function add(a: number, b: number): number;
